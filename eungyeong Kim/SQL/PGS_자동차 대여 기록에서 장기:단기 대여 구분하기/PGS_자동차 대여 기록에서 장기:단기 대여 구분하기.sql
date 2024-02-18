43cf265605f5b00512c45d533b0a719bf0329940
kim eun gyeong <dmsrud518@naver.com>
2024-02-18 15:25:51 +0900

    PGS_자동차 대여 기록에서 장기:단기 대여 구분하기
    PGS_자동차 대여 기록에서 장기:단기 대여 구분하기

---
+++ eungyeong Kim/SQL/PGS_자동차 대여 기록에서 장기:단기 대여 구분하기/PGS_자동차 대여 기록에서 장기:단기 대여 구분하기.sql
@@ -0,0 +1,9 @@
+-- 코드를 입력하세요
+SELECT history_id, car_id, date_format(start_date,"%Y-%m-%d") as start_date , date_format(end_date, "%Y-%m-%d") as end_date,
+(case when (datediff(end_date, start_date)+1) >= 30 then "장기 대여" else "단기 대여" end) as rent_type
+
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+where 1=1
+and month(start_date) = "9"
+and year(start_date) ="2022"
+order by history_id DESC
