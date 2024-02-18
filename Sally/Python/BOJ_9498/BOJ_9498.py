0b2042a2fc4744281a7898c55e835709d0989e6e
seohee <61724682+heeseo11@users.noreply.github.com>
2024-02-18 20:34:48 +0900

    Create BOJ_9498.py

---
+++ Sally/Python/BOJ_9498/BOJ_9498.py
@@ -0,0 +1,12 @@
+A = int(input())
+
+if 90<=A and A<=100:
+  print("A")
+elif 80<=A and A<=89:
+  print("B")
+elif 70<=A and A<=79:
+  print("C")
+elif 60<=A and A<=69:
+  print("D")
+else:
+   print("F") 
