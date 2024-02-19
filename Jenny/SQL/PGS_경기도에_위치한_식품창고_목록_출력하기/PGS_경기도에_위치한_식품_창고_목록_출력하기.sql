b5d6dd6deb90ec61e93d97c2bd0a6846d6fc14ed
Jenny-Jo <38550304+Jenny-Jo@users.noreply.github.com>
2024-02-20 02:39:41 +0900

    Create PGS_경기도에_위치한_식품_창고_목록_출력하기 5분

---
+++ Jenny/SQL/PGS_경기도에_위치한_식품창고_목록_출력하기/PGS_경기도에_위치한_식품_창고_목록_출력하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT warehouse_id,warehouse_name,address,
+case when freezer_yn is null then 'N' else freezer_yn 
+end as freezer_yn
+from food_warehouse
+where address like '%경기도%'
+order by warehouse_id asc
