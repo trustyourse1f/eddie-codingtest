cef08eb5d5ae7f3244e1a6098111ec7ff2bc4de8
trustyourse1f <kissday98@nate.com>
2024-02-20 21:25:43 +0900

    edit file name

--- eungyeong Kim/SQL/PGS_12세 이하인 여자 환자 목록 출력하기/PGS_12세 이하인 여자 환자 목록 출력하기.sql
+++
@@ -1,7 +0,0 @@
--- 코드를 입력하세요
-SELECT pt_name, pt_no, gend_cd, age, ifnull(tlno, 'NONE') as TLNO
-from patient
-where 1=1 
-and age <= 12
-and gend_cd = 'W'
-order by age desc, pt_name
--- eungyeong Kim/SQL/PGS_나이 정보가 없는 회원 수 구하기/README.md
+++
@@ -1,1 +0,0 @@
-문제 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/131528
