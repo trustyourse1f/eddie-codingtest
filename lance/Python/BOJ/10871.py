b6b021591b88e18c296e768707a3dd9582abf3ee
trustyourse1f <kissday98@nate.com>
2024-03-04 13:22:02 +0900

    .DS_Store

--- lance/Python/BOJ/10871.py
+++
@@ -1,7 +0,0 @@
-n, x = map(int, input().split(' '))
-
-arr = list(map(int, input().split(' ')))
-
-for i in range(n):
-    if arr[i] < x :
-        print(arr[i], end=' ')
--- lance/Python/BOJ/2588.py
+++
@@ -1,2 +0,0 @@
-a, b = int(input('')), input('')
-print(*[a*int(x) for x in b][::-1], a*int(b))
