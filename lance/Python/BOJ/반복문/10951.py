5450a4347cf15291a6f2c08665bee00524d24257
trustyourse1f <kissday98@nate.com>
2024-03-04 15:02:14 +0900

    BOJ 10951 검토

--- lance/Python/BOJ/반복문/10951.py
+++ lance/Python/BOJ/반복문/10951.py
@@ -1,5 +1,12 @@
-test_cases = input().split('\n')
+# test_cases = input().split('\n')
 
-for test_case in test_cases:
-    a, b = map(int, test_case.split(' '))
-    print(a+b)
+# for test_case in test_cases:
+#     a, b = map(int, test_case.split(' '))
+#     print(a+b)
+
+while True:
+    try:
+        A, B = map(int, input().split())
+        print(A+B)
+    except:
+        break
