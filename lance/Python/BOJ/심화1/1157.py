1ff04d0ac873fec0b8767f3381a3f52abab69749
trustyourse1f <kissday98@nate.com>
2024-03-11 22:52:39 +0900

    'BOJ 1157 시간 초과'

--- lance/Python/BOJ/심화1/1157.py
+++ lance/Python/BOJ/심화1/1157.py
@@ -1,12 +1,19 @@
 # 9시 40분 시작
 
-str = input()
+word = input() # 입력
 
-str = str.upper()
+word = word.upper() # 모든 문자 대문자로
 
-count_dict = {}
+count_dict = {} # 딕셔너리 생성
 
-for i in str:
-    count_dict[i] = str.count(i)
+for letter in word:  # 단어 한글자씩 돌면서 순회
+    count_dict[letter] = word.count(letter) # 단어별 등장횟수 딕셔너리에 저장 {'M':1, 'I':4, 'S':4, 'P':1}
 
-print(max(count))
+
+# 등장횟수가 가장 많은 글자 추출
+max_letter = [key for key, value in count_dict.items() if value == max(count_dict.values())]
+
+if len(max_letter) != 1:
+    print('?')
+else:
+    print(*max_letter)
