9be60c4300d063d3566ab833e516fbb471666b48
trustyourse1f <kissday98@nate.com>
2024-03-15 21:45:15 +0900

    BOJ 1157

--- lance/Python/BOJ/심화1/1157.py
+++ lance/Python/BOJ/심화1/1157.py
@@ -1,19 +1,13 @@
-# 9시 40분 시작
+word = input().upper()  # MISSISSIPI
+word_list = list(set(word)) # [M, I, S, P]
 
-word = input() # 입력
+cnt = []
+for i in word_list: # M, I, S, P
+  count = word.count(i) # 1, 4, 4, 1
+  cnt.append(count)
 
-word = word.upper() # 모든 문자 대문자로
+if cnt.count(max(cnt)) > 1:
+  print("?")
 
-count_dict = {} # 딕셔너리 생성
-
-for letter in word:  # 단어 한글자씩 돌면서 순회
-    count_dict[letter] = word.count(letter) # 단어별 등장횟수 딕셔너리에 저장 {'M':1, 'I':4, 'S':4, 'P':1}
-
-
-# 등장횟수가 가장 많은 글자 추출
-max_letter = [key for key, value in count_dict.items() if value == max(count_dict.values())]
-
-if len(max_letter) != 1:
-    print('?')
 else:
-    print(*max_letter)
+  print(word_list[(cnt.index(max(cnt)))])
